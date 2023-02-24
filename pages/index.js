36789870b71dfa5842acc3cbf40895e6ac620448
Alex Trost <19617280+a-trost@users.noreply.github.com>
2023-02-24 16:51:09 -0500

    fix: change class to className

--- pages/index.js
+++ pages/index.js
@@ -10,14 +10,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div class="bg-white py-12">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div class="md:flex md:items-center md:justify-between">
-            <div class="md:w-1/2">
-              <p class="text-7xl font-bold leading-tighter mb-4 text-green-600 ">
+      <div className="bg-white py-12">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="md:flex md:items-center md:justify-between">
+            <div className="md:w-1/2">
+              <p className="text-7xl font-bold leading-tighter mb-4 text-green-600 ">
                 Offset Your Carbon Footprint
               </p>
-              <p class="text-lg mb-8 text-gray-700">
+              <p className="text-lg mb-8 text-gray-700">
                 GreenGenie is the easy way to reduce your carbon footprint and
                 help fight climate change. Our app makes it simple and
                 affordable to offset your carbon emissions from daily activities
@@ -25,12 +25,12 @@ export default function Home() {
               </p>
               <a
                 href="#"
-                class="bg-green-500 inline-block mb-6 text-white font-medium py-2 px-4 rounded hover:bg-green-600 hover:text-white transition-colors duration-300"
+                className="bg-green-500 inline-block mb-6 text-white font-medium py-2 px-4 rounded hover:bg-green-600 hover:text-white transition-colors duration-300"
               >
                 Sign Up
               </a>
             </div>
-            <div class="md:w-1/2">
+            <div className="md:w-1/2">
               <img src="/wind-turbine.png" />
             </div>
           </div>
